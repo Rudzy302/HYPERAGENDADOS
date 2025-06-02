cba47823b90396cbca920648b39f9311f6475bb7
Rudzy302 <rudzy302@gmail.com>
2025-06-02 13:24:02 -0500

    cors 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,13 +14,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+    credentials: true,
+  })
 );
-app.use(express.json());
+
 
 // Definir rutas
 app.use("/api/auth", authRoutes);
